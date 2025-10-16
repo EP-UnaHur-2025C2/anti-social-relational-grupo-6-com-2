48fdc0db6f51018cda4a9346fd5672fd377e4b83
FernandoFolcher <ferfolcher@gmail.com>
2025-10-16 04:41:34 -0300

    Agregadas las variables de entorno

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 const express = require ('express')
-const app = express()
-const PORT = 3000 //Hacerla variable de entorno
 const db = require('./db/models') 
 const routes = require('./routes/routes_index')
+require('dotenv').config()
+const app = express()
+
 
 app.use(express.json());
+
 app.use('/api', routes);
+
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, async () => {
     console.log(`El servidor esta corriendo en el puerto ${PORT}`)
     await db.sequelize.sync()
-}) 
+})
